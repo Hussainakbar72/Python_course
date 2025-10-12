03ed02efe743d34a69c4d2ff3be6487a92cd0b38
Hussainakbar72 <hussainakber353@gmail.com>
2025-10-12 14:43:20 +0500

    Add files via upload

---
+++ string_built_in functions.py
@@ -0,0 +1,35 @@
+name = "my name is hussain akbar  "
+
+print(name.capitalize()) # first letter of the string becomes capital
+
+print(name.title())  # first letter of every word in string becomes captal
+
+print(name.split()) # break the string into small parts an return a list
+
+print(name.isnumeric())  # check string is in numeric or not
+
+print(name.strip())   # remove spaces 
+
+print(name.rstrip()) # remove spaces from right side
+
+print(name.lstrip()) # remove spaes from let side
+
+print(name.upper())  # repace all the string in uppercase
+
+print(name.lower()) # covert string in loower case
+
+print(name.islower()) # check string is in lower case
+
+
+
+
+
+
+
+
+# print(x.capitalize()) # first character of first word is capital
+# # print(x.title()) #  first character of ever word is capital
+# # print(x.strip()) # all words in small character
+# # print(x.rstrip()) # remove space in right side
+# # print(x.lstrip()) # remove space in left side
+# # print(x.split())# all words written in quetatioins
